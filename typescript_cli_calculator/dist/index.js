707aea6a02c8d94f44ddc4189105501a8e3ffd46
Muhammad Taha <tahakhan26402@gmail.com>
2023-08-01 12:30:49 -0700

    feat: complete number guessing game

--- typescript_cli_calculator/dist/index.js
+++ typescript_cli_calculator/dist/index.js
@@ -1,3 +1,5 @@
+#! /usr/bin/env node
+
 import Inquirer from "inquirer";
 import chalk from "chalk";
 console.log(chalk.blue(`
---
+++ typescript_cli_number_guessing_game/dist/index.js
@@ -0,0 +1,34 @@
+import Inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.blue(`
+
+------------------------------------------
+
+            Number Guessing Game
+
+------------------------------------------
+
+`));
+const guessingGame = async () => {
+    let score = 0;
+    let status = true;
+    while (status) {
+        const randomNumber = Math.floor(Math.random() * 10) + 1;
+        const { guessedNumber } = await Inquirer.prompt([
+            {
+                name: "guessedNumber",
+                type: 'number',
+                message: "Enter a number between 1 to 10 : "
+            },
+        ]);
+        if (guessedNumber === randomNumber) {
+            score += 10;
+            console.log(chalk.green(`Congratulation..! you guessed it right... your score is : ${chalk.blue(score)}`));
+        }
+        else {
+            console.log(chalk.red(`Sorry..! wrong guessed try again... your score is : ${chalk.greenBright(score)}`));
+            status = false;
+        }
+    }
+};
+guessingGame();
