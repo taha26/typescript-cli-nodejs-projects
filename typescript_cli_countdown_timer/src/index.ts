2be64be5c749bf4406c49894ddcee92ef0328e46
Muhammad Taha <tahakhan26402@gmail.com>
2023-08-07 14:57:26 -0700

    feat: complete countdown timer project

---
+++ typescript_cli_countdown_timer/src/index.ts
@@ -0,0 +1,57 @@
+import Inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.blue(`
+
+------------------------------------------
+            Countdown Timer
+------------------------------------------
+
+`));
+
+(async () => {
+
+    const { userInput }: { userInput: string } = await Inquirer.prompt([
+        {
+            name: "userInput",
+            type: "input",
+            message: "Enter a date and time e.g: 3 july 2020 14:10:00 : ",
+            validate: (input: string) => {
+                if (!input) {
+                    return "Invaild input"
+                }
+                else {
+                    return true
+                }
+            }
+        }
+    ]);
+
+    const countDownDate = new Date(userInput).getTime();
+    const interval = setInterval(function () {
+
+        // Get today's date and time
+        const now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        const distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(interval);
+            console.log(chalk.red("EXPIRED"))
+            return
+        }
+
+        // Display the result
+        console.log(chalk.green(days + "d " + hours + "h " + minutes + "m " + seconds + "s "))
+
+    }, 1000);
+}
+)()
